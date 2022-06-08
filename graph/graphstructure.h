55e17b7ded4a0f629fbf2fdc0537f0f45e41e3e0
roket64 <1p41i4y1@gmail.com>
2022-06-09 03:38:32 +0900

    Changes

--- graph/graphstructure.h
+++ graph/graphstructure.h
@@ -3,75 +3,54 @@
 
 #include <cstdlib>
 #include <string>
+#include <cassert>
 #include <type_traits>
 
 template<class T1, class T2>
 class Node {
 private:
-    // can use all integral types but not floating point or bool type
-    using ntype = typename std::enable_if_t
-            <!(std::is_floating_point<T1>::value
-               || std::is_same<bool, T1>::value)
-             && (std::is_integral<T1>::value
-                 || std::is_same<std::string, T1>::value), T1>;
+    static_assert(!(std::is_floating_point<T1>::value
+                    || std::is_same<bool, T1>::value));
 
-    // can use all integral and floating point types but not unsigned or bool, char, string type
-    using wtype = typename std::enable_if_t
-            <!(std::is_unsigned<T2>::value
-               || std::is_same<bool, T2>::value
-               || std::is_same<char, T2>::value
-               || std::is_same<char16_t, T2>::value
-               || std::is_same<char32_t, T2>::value
-               || std::is_same<std::string, T2>::value)
-             && (std::is_arithmetic<T2>::value), T2>;
+    static_assert(!(std::is_unsigned<T2>::value
+                    || std::is_same<bool, T2>::value
+                    || std::is_same<char, T2>::value
+                    || std::is_same<char16_t, T2>::value
+                    || std::is_same<char32_t, T2>::value
+                    || std::is_same<std::string, T2>::value));
 
-    ntype node;
-    wtype weight;
+    T1 m_node;
+    T2 m_weight;
 
 public:
     Node(T1 _Node, T2 _Weight = 1)
-            : node(_Node), weight(_Weight) {}
+            : m_node(_Node), m_weight(_Weight) {};
 
-    ntype GetNode() { return node; }
+    T1 node() { return m_node; }
 
-    wtype GetWeight() { return weight; }
+    T2 weight() { return m_weight; }
 
-    // in priority queue, this gives higher priority to lower weight edge
-    bool operator<(const Node &n) const { return n.weight < weight; }
+    bool operator<(const Node &n) const { return n.m_weight < m_weight; }
 
     ~Node() {}
-};
-
-template<class T>
-class Edge {
-private:
-    T from;
-    T to;
-    long long weight;
-
-public:
-    Edge(T _From, T _To, long long _Weight = 1)
-            : from(_From), to(_To), weight(_Weight) {}
-
-    ~Edge() {}
 };
 
 template<class T1, class T2>
 class TreeNode : Node<T1, T2> {
 private:
-    Node<T1, T2> parent;
-    Node<T1, T2> left_child;
-    Node<T1, T2> right_child;
+    Node<T1, T2> m_parent;
+    Node<T1, T2> m_leftchild;
+    Node<T1, T2> m_rightchild;
 
 public:
-    TreeNode(T1 _Id, T1 _Parent, T1 _Leftchild, T1 _Rightchild, T2 _Weight)
-            : Node<T1, T2>(_Id, _Weight), parent(_Parent), left_child(_Leftchild), right_child(_Rightchild) {}
+    TreeNode(T1 _Id, T1 _Parent, T1 _Leftchild, T1 _Rightchild, T2 _Weight = 1)
+            : Node<T1, T2>(_Id, _Weight), m_parent(_Parent), m_leftchild(_Leftchild), m_rightchild(_Rightchild) {};
 
-    Node<T1, T2> GetParent() { return parent; }
+    Node<T1, T2> parent() { return m_parent; }
 
-    Node<T1, T2> Getlc() { return left_child; }
+    Node<T1, T2> lc() { return m_leftchild; }
 
-    Node<T1, T2> Getrc() { return right_child; }
+    Node<T1, T2> rc() { return m_rightchild; }
 
     ~TreeNode() {}
 };
@@ -79,29 +58,50 @@ public:
 template<class T1, class T2>
 class TreapNode : Node<T1, T2> {
 private:
-    T1 priority;
+    long long m_priority;
 
 public:
     TreapNode(T1 _Id, T2 _Weight)
-            : Node<T1, T2>(_Id, _Weight), priority(rand()) {}
+            : Node<T1, T2>(_Id, _Weight), m_priority(rand()) {};
 
-    ~TreapNode() {}
+    ~TreapNode() {};
+};
+
+template<class T>
+class Edge {
+private:
+    static_assert(!(std::is_floating_point<T>::value
+                    || std::is_same<bool, T>::value));
+
+    T m_from;
+    T m_to;
+    long long weight;
+
+public:
+    Edge(T _From, T _To, long long _Weight = 1)
+            : m_from(_From), m_to(_To), weight(_Weight) {};
+
+    ~Edge() {}
 };
 
 template<class T>
 class Point {
 private:
-    T x;
-    T y;
-    long long weight;
+    static_assert(!(std::is_same<bool, T>::value
+                    || std::is_same<char, T>::value
+                    || std::is_same<char16_t, T>::value
+                    || std::is_same<char32_t, T>::value
+                    || std::is_same<std::string, T>::value));
+
+    T m_x;
+    T m_y;
+    T m_weight;
 
 public:
-    Point(T _X, T _Y, long long _Weight)
-            : x(_X), y(_Y), weight(_Weight) {}
+    Point(T _X, T _Y, T _Weight)
+            : m_x(_X), m_y(_Y), m_weight(_Weight) {};
 
-    ~Point() {}
+    ~Point() {};
 };
-
-/**/
 
 #endif //UNTITLED_GRAPHSTRUCTURE_H
