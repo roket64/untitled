6ffa2cf804a4f6b28be95fbb6442f23e7d11fcb3
roket64 <1p41i4y1@gmail.com>
2022-07-07 12:05:12 +0900

    segment tree fix

--- data_structure/segtree/segtree_lazy.h
+++ data_structure/segtree/segtree_lazy.h
@@ -4,19 +4,22 @@
 #include <vector>
 #include <math.h>
 
-template<class T>
+template<class ArrayType, class ValueType>
 class Segment_tree_lazy {
 private:
-    std::vector<T> m_arr;
-    std::vector<T> m_tree;
-    std::vector<T> m_lazy;
+    typedef ArrayType A;
+    typedef ValueType V;
 
-    T init(std::vector<T> arr_, int idx_, int start_, int end_) {
+    std::vector<A> m_arr;
+    std::vector<A> m_tree;
+    std::vector<A> m_lazy;
+
+    A init(std::vector<ArrayType> arr_, int idx_, int start_, int end_) {
         if (start_ == end_) return m_tree[idx_] = arr_[start_];
 
         int mid = (start_ + end_) / 2;
-        T lhs = init(arr_, 2 * idx_, start_, mid);
-        T rhs = init(arr_, 2 * idx_ + 1, mid + 1, end_);
+        A lhs = init(arr_, 2 * idx_, start_, mid);
+        A rhs = init(arr_, 2 * idx_ + 1, mid + 1, end_);
 
         return m_tree[idx_] = lhs + rhs;
     }
@@ -32,7 +35,7 @@ private:
     }
 
 public:
-    Segment_tree_lazy(std::vector<T> &arr_) {
+    explicit Segment_tree_lazy(std::vector<A> &arr_) {
         int sz = arr_.size();
 //        m_arr.assign(arr_.begin(), arr_.end());
         m_tree.assign(1 << (int) ceil(log2(sz) + 1), 0);
@@ -40,7 +43,7 @@ public:
         init(arr_, 1, 0, sz - 1);
     }
 
-    T update(int idx_, int start_, int end_, int left_, int right_, T diff_) {
+    A update(int idx_, int start_, int end_, int left_, int right_, V diff_) {
         propagate(idx_, start_, end_);
         if (right_ < start_ || end_ < left_) return m_tree[idx_];
         if (left_ <= start_ && end_ <= right_) {
@@ -53,20 +56,20 @@ public:
         }
 
         int mid = (start_ + end_) / 2;
-        T lhs = update(2 * idx_, start_, mid, left_, right_, diff_);
-        T rhs = update(2 * idx_ + 1, mid + 1, end_, left_, right_, diff_);
+        A lhs = update(2 * idx_, start_, mid, left_, right_, diff_);
+        A rhs = update(2 * idx_ + 1, mid + 1, end_, left_, right_, diff_);
 
         return m_tree[idx_] = lhs + rhs;
     }
 
-    T query(int idx_, int start_, int end_, int left_, int right_) {
+    A query(int idx_, int start_, int end_, int left_, int right_) {
         propagate(idx_, start_, end_);
         if (right_ < start_ || end_ < left_) return 0;
         if (left_ <= start_ && end_ <= right_) return m_tree[idx_];
 
         int mid = (start_ + end_) / 2;
-        T lhs = query(2 * idx_, start_, mid, left_, right_);
-        T rhs = query(2 * idx_ + 1, mid + 1, end_, left_, right_);
+        A lhs = query(2 * idx_, start_, mid, left_, right_);
+        A rhs = query(2 * idx_ + 1, mid + 1, end_, left_, right_);
 
         return lhs + rhs;
     }
